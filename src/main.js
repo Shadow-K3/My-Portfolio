251fdf0dae46da0783c1d105b945bb35f51ea3ac
Shadow-K3 <tchoumiberol816@gmail.com>
2026-03-25 19:35:06 +0100

    chore(debug): log i18n locale and availableLocales in browser

--- src/main.js
+++ src/main.js
@@ -21,3 +21,15 @@ if (typeof window !== 'undefined' && window.localStorage) {
 	}
 }
 
+// Debug: log i18n state to help diagnose missing translations in production
+if (typeof window !== 'undefined') {
+  try {
+    // eslint-disable-next-line no-console
+    console.info('i18n: locale=', i18n.global?.locale?.value ?? i18n.global?.locale);
+    // eslint-disable-next-line no-console
+    console.info('i18n: availableLocales=', i18n.global?.availableLocales || Object.keys(i18n.global?.messages || {}));
+  } catch (err) {
+    // ignore
+  }
+}
+
